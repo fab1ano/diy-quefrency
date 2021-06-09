5c6fa68f963388c54f0690d97c63047976a9611d
fabiano <fabian.fleischer@fau.de>
2021-06-09 16:30:09 +0200

    Move audio control to desired position

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -17,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [FN1] = LAYOUT_60_iso(
-    KC_TILD, KC_F1,         KC_F2,       KC_F3,      KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,     KC_F11,  KC_F12,  KC_DEL,   KC_BSPC,
-    RGB_TOG, RGB_MOD,       KC_BSPACE,   KC_UP,      KC_DEL,   _______, _______,  KC_PGUP, KC_UP,   KC_PGDOWN, KC_PSCREEN, _______, _______,
-    _______, RGB_PLAIN_RED, KC_LEFT,     KC_DOWN,    KC_RGHT,  KC_ENT,  _______,  KC_LEFT, KC_DOWN, KC_RGHT,   _______,    _______, _______, _______,
-    _______, _______,       KC__VOLDOWN, KC__VOLUP,  KC__MUTE, _______, _______,  _______, _______, _______,   _______,    _______, _______, _______,
-    KC_TILD, _______,       _______,     _______,    _______,           _______,  _______, _______, _______,   _______,    _______, _______
+    KC_TILD, KC_F1,         KC_F2,     KC_F3,       KC_F4,     KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,     KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
+    RGB_TOG, RGB_MOD,       KC_BSPACE, KC_UP,       KC_DEL,    _______,  _______, KC_PGUP, KC_UP,   KC_PGDOWN, KC_PSCREEN, _______, _______,
+    _______, RGB_PLAIN_RED, KC_LEFT,   KC_DOWN,     KC_RGHT,   KC_ENT,   _______, KC_LEFT, KC_DOWN, KC_RGHT,   _______,    _______, _______, _______,
+    _______, _______,       _______,   KC__VOLDOWN, KC__VOLUP, KC__MUTE, _______, _______, _______, _______,   _______,    _______, _______, _______,
+    KC_TILD, _______,       _______,   _______,     _______,             _______, _______, _______, _______,   _______,    _______, _______
   )
 };
 
